dad014a1d2e332c5969bb4f2d3badb46318a5e9d
Muath Alshathri <abogbl4@gmail.com>
2023-12-21 11:41:05 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+// import * as pdfjsLib from 'pdfjs-dist';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  sanitizer = inject(DomSanitizer);
+
   title = 'pdf-viewer';
+
+  pdfViewerUrl?: SafeResourceUrl;
+
+  pathToViewer4 = 'assets/pdfjs-4.0.269-dist/web/viewer.html';
+
+  // pdfWorkerSourcePath = 'src/assets/pdfjs-3.11.174-dist/build/pdf.worker.*';
+
+  buildingSystemPdf =
+    'https://mapservice.alriyadh.gov.sa/BuildingSystem/building-code-report-experimental?parcelId=14289493';
+
+  dummyPdf =
+    'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+
+  dummyPdf2Pages = 'https://www.africau.edu/images/default/sample.pdf';
+
+  ngOnInit() {
+    this.pdfViewerUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `${this.pathToViewer4}?file=${encodeURIComponent(this.dummyPdf2Pages)}`
+    );
+  }
 }
+
+// pdfjsLib.GlobalWorkerOptions.workerSrc = this.pdfWorkerSourcePath;
+
+/* 
+"assets": [
+              "src/favicon.ico",
+              "src/assets",
+              {
+                "glob": "pdf.worker.*",
+                "input": "node_modules/pdfjs-dist/build/",
+                "output": ""
+              } 
+            ]
+*/
